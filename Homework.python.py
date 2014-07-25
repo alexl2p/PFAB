af407ba3be2ccd98e721e56071d60af1a2771695
Alex <alex@learntoprogram.tv>
2014-07-25 15:05:12 -0400

    Homework 1
    Homework for PFAB

---
+++ Homework.python.py
@@ -0,0 +1,39 @@
+first = "Alex"
+last = "O."
+age = 25
+
+print first, last, age
+
+print "------------------------"
+
+sideA = 12.55
+sideB = 17.85
+
+sum = (sideA * sideA) + (sideB * sideB)
+
+import math 
+
+sideC = math.sqrt(sum)
+
+print "side C = ", sideC
+
+print "------------------------"
+
+operand1 = 95
+operand2 = 64.5
+
+addition = operand1 + operand2
+subtraction = operand1 - operand2
+mult = operand1 * operand2
+division = operand1 / operand2
+mod = operand1 % operand2
+
+print operand1, "+", operand2, "=", addition
+print operand1, "-", operand2, "=", subtraction
+print operand1, "*", operand2, "=", mult
+print operand1, "/", operand2, "=", division
+print operand1, "%", operand2, "=", mod, "(<-this is the remainder after division)"
+
+print "------------------------"
+
+#Just replace the value of the operand variables and the equation will still work.
